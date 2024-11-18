559d240eaef12b4927daba01aa1a8cb19923aab1
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-11-18 10:53:27 +0900

    서버연결파일업로드

---
+++ secret.js
@@ -0,0 +1,7 @@
+
+const dbUrl = 'mongodb+srv://sparta-user:aaaa4321@express-mongo.p1x5z.mongodb.net/?retryWrites=true&w=majority&appName=express-mongo';
+const dbName = 'roguelike';
+export {
+    dbUrl,
+    dbName,
+}
